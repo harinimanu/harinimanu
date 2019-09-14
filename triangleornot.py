3dbd5edfc750ab183e751e3ad4ed526f35a78080
harinimanu123 <55320732+harinimanu123@users.noreply.github.com>
2019-09-14 19:57:58 +0530

    Update triangleornot.py

--- triangleornot.py
+++ triangleornot.py
@@ -2,12 +2,15 @@ def checkValidity(a, b, c):
       
     # check condition  
     if (a + b <= c) or (a + c <= b) or (b + c <= a) : 
-        print("yes")
+        return False
     else: 
-        print("no")        
+        return True        
   
 # driver code  
 a = 7
 b = 10
 c = 5
-print(checkValidity(a, b, c)) 
+if checkValidity(a, b, c): 
+    print("Valid")  
+else: 
+    print("Invalid") 
