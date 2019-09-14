09f2c788b96375eea092096f322f8fcfcfd6b9b8
harinimanu123 <55320732+harinimanu123@users.noreply.github.com>
2019-09-14 19:47:33 +0530

    Add files via upload

---
+++ triangleornot.py
@@ -0,0 +1,16 @@
+def checkValidity(a, b, c):  
+      
+    # check condition  
+    if (a + b <= c) or (a + c <= b) or (b + c <= a) : 
+        return False
+    else: 
+        return True        
+  
+# driver code  
+a = 7
+b = 10
+c = 5
+if checkValidity(a, b, c): 
+    print("yes")  
+else: 
+    print("no") 
