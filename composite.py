70bef92a1b6618a04997c2ee401257c1544a272a
harinimanu123 <55320732+harinimanu123@users.noreply.github.com>
2019-09-16 00:07:59 +0530

    Add files via upload

---
+++ composite.py
@@ -0,0 +1,24 @@
+def isComposite(n): 
+  
+    # Corner cases 
+    if (n <= 1): 
+        return False
+    if (n <= 3): 
+        return False
+  
+    # This is checked so that we can skip  
+    # middle five numbers in below loop 
+    if (n % 2 == 0 or n % 3 == 0): 
+        return True
+    i = 5
+    while(i * i <= n): 
+          
+        if (n % i == 0 or n % (i + 2) == 0): 
+            return True
+        i = i + 6
+          
+    return False
+  
+# Driver Program to test above function 
+n=int(input()) 
+print("yes") if(isComposite(n)) else print("no") 
