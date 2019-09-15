458b655f40a3b437461c555fd83b58dcbe6a9c3a
harinimanu123 <55320732+harinimanu123@users.noreply.github.com>
2019-09-15 16:09:04 +0530

    Update majestic.py

--- majestic.py
+++ majestic.py
@@ -1,5 +1,6 @@
-a[6]=list(map(int,input().split()))
-if(a[0]+a[1]+a[2]==a[3]+a[4]+a[5]):
+n = int(input())
+a=list(map(int,input().split()))
+if(a[0]+a[1]+a[2]==a[n-1]+a[n-2]+a[n-3]):
     print(1)
 else:
     print(0)
