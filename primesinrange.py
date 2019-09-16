2a3af6ab1036bfd0ccdd7d1be48fe2aff4c3f501
harinimanu123 <55320732+harinimanu123@users.noreply.github.com>
2019-09-16 09:20:21 +0530

    Update primesinrange.py

--- primesinrange.py
+++ primesinrange.py
@@ -1,5 +1,5 @@
 lower,upper = map(int,input().split()) 
-for num in range(lower+1,upper):  
+for num in range(lower,upper+1):  
    if num > 1:
        for i in range(2,num):  
            if (num % i) == 0:  
