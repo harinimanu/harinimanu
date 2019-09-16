04636b25be7d905678f295bca1a8665581f18f0b
harinimanu123 <55320732+harinimanu123@users.noreply.github.com>
2019-09-16 10:36:46 +0530

    Add files via upload

---
+++ productofdigits.py
@@ -0,0 +1,14 @@
+def getProduct(n): 
+  
+    product = 1
+  
+    while (n != 0): 
+        product = product * (n % 10) 
+        n = n // 10
+  
+    return product 
+  
+# Driver Code 
+n=int(input())
+print(getProduct(n)) 
+  
