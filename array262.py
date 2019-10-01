1c6becd49248e03730e04869f1093f7ff93ffe81
harinimanu123 <55320732+harinimanu123@users.noreply.github.com>
2019-10-01 20:02:44 +0530

    Update array262.py

--- array262.py
+++ array262.py
@@ -1,11 +1,7 @@
-n=int(input())
-l=list(map(int,input().split()))
+n,m=map(int,input().split())
 k=list(map(int,input().split()))
-count=0
-for i in range(n):
-  if(l[i] in k):
-    count=count+1
-if(count==n):
-  print("yes")
+if m in k:
+  print("yes",end=" ")
+  print(k.count(m),end=" ")
 else:
   print("no")
