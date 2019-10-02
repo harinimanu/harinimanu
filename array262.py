394acc5527c961387404b54d1597d5668a291c87
harinimanu123 <55320732+harinimanu123@users.noreply.github.com>
2019-10-02 11:29:12 +0530

    Update array262.py

--- array262.py
+++ array262.py
@@ -1,3 +1,21 @@
-n,k=input().split()
-for i in range(int(k)-1,len(n),int(k)):
-  print(n[i],end=" ")
+n,k=map(int,input().split())
+l=list(map(int,input().split()))
+m=[]
+def Remove(duplicate): 
+    final_list = [] 
+    for num in duplicate: 
+        if num not in final_list: 
+            final_list.append(num) 
+    return final_list 
+for i in l:
+  c=l.count(i)
+  if(c==k):
+    m.append(i)
+h=sorted(m)
+if(len(m)!=0):
+  h=Remove(h)
+  for i in h:
+    print(i,end=" ")
+else:
+  print(-1)
+
